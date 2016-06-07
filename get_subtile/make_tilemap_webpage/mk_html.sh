d15dd4fb0f99f4e7373f0b5c0c789396c32730c8
chris hill <chrishill@chris-hills-MacBook-Pro-4.local>
2016-06-06 23:49:06 -0400

    Partial addition for tile web page with indices and coords

--- get_subtile/make_tilemap_webpage/mk_html.sh
+++ get_subtile/make_tilemap_webpage/mk_html.sh
@@ -38,7 +38,7 @@ for j in $(seq 0 23); do
  echo "<tr>"
  for i in $(seq 0  3); do
   fsuf=`printf "%3.3d" $(( ${i} + ${n0} ))`
-  echo "<td> <a href=\"subpanel1080x540_llc4320_map_${fsuf}.jpeg\"><img src=\"subpanel1080x540_llc4320_map_${fsuf}_thumb.jpeg\"></a> </td>"
+  echo "<td> <a href=\"subpanel1080x540_llc4320_map_${fsuf}.html\"><img src=\"subpanel1080x540_llc4320_map_${fsuf}_thumb.jpeg\"></a> </td>"
  done
  echo "</tr>"
  n0=$(($n0-4))
@@ -60,7 +60,7 @@ for j in $(seq 0 31); do
  echo "<tr>"
  for i in $(seq 0  3); do
   fsuf=`printf "%3.3d" $(( ${i} + ${n0} ))`
-  echo "<td> <a href=\"subpanel1080x540_llc4320_map_${fsuf}.jpeg\"><img src=\"subpanel1080x540_llc4320_map_${fsuf}_thumb.jpeg\"></a> </td>"
+  echo "<td> <a href=\"subpanel1080x540_llc4320_map_${fsuf}.html\"><img src=\"subpanel1080x540_llc4320_map_${fsuf}_thumb.jpeg\"></a> </td>"
  done
  echo "</tr>"
  n0=$(($n0-4))
@@ -77,7 +77,7 @@ for j in $(seq 0 15); do
  echo "<tr>"
  for i in $(seq 0  11); do
   fsuf=`printf "%3.3d" $(( ${i} + ${n0} ))`
-  echo "<td> <a href=\"subpanel1080x540_llc4320_map_${fsuf}.jpeg\"><img src=\"subpanel1080x540_llc4320_map_${fsuf}_thumb.jpeg\"></a> </td>"
+  echo "<td> <a href=\"subpanel1080x540_llc4320_map_${fsuf}.html\"><img src=\"subpanel1080x540_llc4320_map_${fsuf}_thumb.jpeg\"></a> </td>"
  done
  echo "</tr>"
  n0=$(($n0-12))
@@ -97,3 +97,90 @@ echo "</table>"
 echo "</body>"
 echo "</html>"
 
+
+npsy=540;
+npsx=1080;
+nx=4320;
+
+
+itile=0
+echo "<table>"
+for fn in $(seq 1 5); do
+ if [ ${fn} -eq 1 ] || [ ${fn} -eq 2 ]; then
+  nxf=$((${nx}  ))
+  nyf=$((${nx}*3))
+ fi
+ if [ ${fn} -eq 3 ]; then
+  nxf=$((${nx}  ))
+  nyf=$((${nx}  ))
+ fi
+ if [ ${fn} -eq 4 ] || [ ${fn} -eq 5 ]; then
+  nxf=$((${nx}*3))
+  nyf=$((${nx}  ))
+ fi
+ npx=$((${nxf}/${npsx}))
+ npy=$((${nyf}/${npsy}))
+ # echo "Face "${fn}" npx="${npx}" npy="${npy}" lval0="${lval0}" bval0="${bval0}
+ 
+ for nty in $(seq 0 $((npy-1))); do
+  for ntx in $(seq 0 $((npx-1))); do
+   itile=$(($itile+1))
+   ilof=$((${ntx}*${npsx}+1))
+   ihif=$((${ilof}+${npsx}-1))
+   jlof=$((${nty}*${npsy}+1))
+   jhif=$((${jlof}+${npsy}-1))
+
+   echo "<tr>"
+   echo "<td>"
+   echo "F"${fn}":("${ilof}","${jhif}")"
+   echo "</td>"
+   echo "<td>"
+   echo "&nbsp;"
+   echo "</td>"
+   echo "<td>"
+   echo "F"${fn}":("${ihif}","${jhif}")"
+   echo "</td>"
+   echo "</tr>"
+
+   itilestr=`printf "%3.3d" $(($itile))`
+   echo "<tr>"
+   echo "<td>"
+   echo "&nbsp;"
+   echo "</td>"
+   echo "<td>"
+   echo "tile =subpanel1080x540_llc4320_map_"${itilestr}".jpeg"
+   echo "</td>"
+   echo "<td>"
+   echo "&nbsp;"
+   echo "</td>"
+   echo "</tr>"
+
+   echo "<tr>"
+   echo "<td>"
+   echo "F"${fn}":("${ilof}","${jlof}")"
+   echo "</td>"
+   echo "<td>"
+   echo "&nbsp;"
+   echo "</td>"
+   echo "<td>"
+   echo "F"${fn}":("${ihif}","${jlof}")"
+   echo "</td>"
+   echo "</tr>"
+
+   echo "<tr>"
+   echo "<td>"
+   echo "-----"
+   echo "</td>"
+   echo "<td>"
+   echo "-----"
+   echo "</td>"
+   echo "<td>"
+   echo "-----"
+   echo "</td>"
+   echo "</tr>"
+
+   itilestr=`printf "%3.3d" $(($itile))`
+  done
+ done
+done
+echo "</table>"
