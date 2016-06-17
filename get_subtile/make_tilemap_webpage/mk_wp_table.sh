c62bc24c3f575e48d043ff3bfd34d81a79f179f7
chris hill <chrishill@chris-hills-MacBook-Pro-4.local>
2016-06-16 22:52:21 -0400

    Added table only code fragment generation

---
+++ get_subtile/make_tilemap_webpage/mk_wp_table.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+cd ${odir}
+
+echo "<table>"
+echo "<tr>"
+echo "<td width=\"22%\">"
+
+echo "<table>"
+
+n0=93;
+for j in $(seq 0 15); do
+ echo "<tr>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+ echo "</tr>"
+done
+for j in $(seq 0 23); do
+ echo "<tr>"
+ for i in $(seq 0  3); do
+  fsuf=`printf "%3.3d" $(( ${i} + ${n0} ))`
+  echo "<td> <a href=\"${wptilepage}?tnum=${fsuf}\"><img class=\"cell_image\" src=\"${wpimgdir}subpanel1080x540_llc4320_map_${fsuf}_thumb.jpeg\"></a> </td>"
+ done
+ echo "</tr>"
+ n0=$(($n0-4))
+done
+
+echo "</table>"
+
+echo "</td>"
+
+echo "<td width=\"22%\">"
+echo "<table>"
+
+n0=221;
+for j in $(seq 0 7); do
+ echo "<tr>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+ echo "</tr>"
+done
+for j in $(seq 0 31); do
+ echo "<tr>"
+ for i in $(seq 0  3); do
+  fsuf=`printf "%3.3d" $(( ${i} + ${n0} ))`
+  echo "<td> <a href=\"${wptilepage}?tnum=${fsuf}\"><img class=\"cell_image\" src=\"${wpimgdir}subpanel1080x540_llc4320_map_${fsuf}_thumb.jpeg\"></a> </td>"
+ done
+ echo "</tr>"
+ n0=$(($n0-4))
+done
+
+echo "</table>"
+echo "</td>"
+
+echo "<td width=\"66%\">"
+echo "<table>"
+
+n0=405;
+for j in $(seq 0 15); do
+ echo "<tr>"
+ for i in $(seq 0  11); do
+  fsuf=`printf "%3.3d" $(( ${i} + ${n0} ))`
+  echo "<td> <a href=\"${wptilepage}?tnum=${fsuf}\"><img class=\"cell_image\" src=\"${wpimgdir}subpanel1080x540_llc4320_map_${fsuf}_thumb.jpeg\"></a> </td>"
+ done
+ echo "</tr>"
+ n0=$(($n0-12))
+done
+for j in $(seq 0 23); do
+ echo "<tr>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+  echo "<td> <img class=\"cell_image\" src=\"${wpimgdir}white_pad.jpeg\"> </td>"
+ echo "</tr>"
+done
+# </tr>
+
+echo "</table>"
+echo "</td>"
+
+echo "</tr>"
+echo "</table>"
+
