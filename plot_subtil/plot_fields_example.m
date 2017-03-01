9ec57ce20406b737a72cb15eef9feb85de37665d
Chris Hill <christophernhill@gmail.com>
2017-03-01 09:32:50 -0500

    Add root path name parameter

--- plot_subtil/plot_fields_example.m
+++ plot_subtil/plot_fields_example.m
@@ -1,13 +1,15 @@
 pNo=372;
-itNo=486864;
+pNo=278;
+itNo=685152;
 kLev=30;
+pRoot='/nfs/cnhlab002/cnh/llc4320/tiled_output/'
 
-dName=sprintf( '../p_%3.3d/grid/Depth.p_%4.4d.data',pNo,pNo);
-xcName=sprintf('../p_%3.3d/grid/XC.p_%4.4d.data',pNo,pNo);
-ycName=sprintf('../p_%3.3d/grid/YC.p_%4.4d.data',pNo,pNo);
-xgName=sprintf( '../p_%3.3d/grid/XG.p_%4.4d.data',pNo,pNo);
-ygName=sprintf( '../p_%3.3d/grid/YG.p_%4.4d.data',pNo,pNo);
-rcName=sprintf( '../p_%3.3d/grid/RC.p_%4.4d.data',pNo,pNo);
+dName=sprintf( '%s/p_%3.3d/grid/Depth.p_%4.4d.data',pRoot,pNo,pNo);
+xcName=sprintf('%s/p_%3.3d/grid/XC.p_%4.4d.data',pRoot,pNo,pNo);
+ycName=sprintf('%s/p_%3.3d/grid/YC.p_%4.4d.data',pRoot,pNo,pNo);
+xgName=sprintf( '%s/p_%3.3d/grid/XG.p_%4.4d.data',pRoot,pNo,pNo);
+ygName=sprintf( '%s/p_%3.3d/grid/YG.p_%4.4d.data',pRoot,pNo,pNo);
+rcName=sprintf( '%s/p_%3.3d/grid/RC.p_%4.4d.data',pRoot,pNo,pNo);
 
 dte=datestr(datenum(2011,9,10)+itNo*25/60/60/24,-1);
 
@@ -48,7 +50,7 @@ colorbar
 
 subplot(3,4,2)
 
-thetaName=sprintf( '../p_%3.3d/Theta/i_%15.15d/Theta.%10.10d.p_%4.4d.k_%4.4d.data',pNo,itNo,itNo,pNo,kLev);
+thetaName=sprintf( '%s/p_%3.3d/i_%15.15d/Theta/Theta.%10.10d.p_%4.4d.k_%4.4d.data',pRoot,pNo,itNo,itNo,pNo,kLev);
 
 fid=fopen(thetaName,'r','ieee-be');
 thVals=fread(fid,[1080 540],'float32');
@@ -69,7 +71,7 @@ colorbar
 
 subplot(3,4,3)
 
-etaName=sprintf( '../p_%3.3d/Eta/i_%15.15d/Eta.%10.10d.p_%4.4d.k_%4.4d.data',pNo,itNo,itNo,pNo,1);
+etaName=sprintf( '%s/p_%3.3d/i_%15.15d/Eta/Eta.%10.10d.p_%4.4d.k_%4.4d.data',pRoot,pNo,itNo,itNo,pNo,1);
 
 fid=fopen(etaName,'r','ieee-be');
 etaVals=fread(fid,[1080 540],'float32');
@@ -87,7 +89,7 @@ colorbar
 
 subplot(3,4,4)
 
-uName=sprintf( '../p_%3.3d/U/i_%15.15d/U.%10.10d.p_%4.4d.k_%4.4d.data',pNo,itNo,itNo,pNo,kLev);
+uName=sprintf( '%s/p_%3.3d/i_%15.15d/U/U.%10.10d.p_%4.4d.k_%4.4d.data',pRoot,pNo,itNo,itNo,pNo,kLev);
 
 fid=fopen(uName,'r','ieee-be');
 uVals=fread(fid,[1080 540],'float32');
@@ -108,7 +110,7 @@ colorbar
 
 subplot(3,4,5)
 
-vName=sprintf( '../p_%3.3d/V/i_%15.15d/V.%10.10d.p_%4.4d.k_%4.4d.data',pNo,itNo,itNo,pNo,kLev);
+vName=sprintf( '%s/p_%3.3d/i_%15.15d/V/V.%10.10d.p_%4.4d.k_%4.4d.data',pRoot,pNo,itNo,itNo,pNo,kLev);
 
 fid=fopen(vName,'r','ieee-be');
 vVals=fread(fid,[1080 540],'float32');
